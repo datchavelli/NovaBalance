b30b8f68844fe986ebbee8f2208e3db1aa541122
Danilo Zdravkovic <dan.zdravkovic97@gmail.com>
2025-02-27 23:02:10 +0100

    JS changes and modal

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $("body").on("click","#readMore", function(e){
         e.preventDefault();
         $("#homeModal").modal("show");
-    })
+    });
+
+    $("body").on("click", ".close", function(e){
+        e.preventDefault();
+        $("#homeModal").modal("hide");
+    });
 });
--- views/home/modal.mvc.php
+++ views/home/modal.mvc.php
@@ -1,19 +1,20 @@
 <!-- Modal -->
 <div class="modal fade" id="homeModal" tabindex="-1" role="dialog" aria-labelledby="homeModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="homeModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="homeModalLabel">Funkcionalnosti - Više Detalja</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        Detaljan pregled svih funkcionalnosti koje vam štede vreme i resurse.
+        Od automatskog obračuna zarada do kreiranja Arhivske knjige i slanja elektronskih faktura
+        putem DoCloop sistema - svaki alat u <b><span class="logo-style nova">Nova</span><span class="logo-style forma">Forma</span></b> softveru dizajniran je da vam uštedi vreme i
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary close" data-dismiss="modal">Zatvori</button>
       </div>
     </div>
   </div>
